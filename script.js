956cfc2a981799ae1787fad49bb1508749152460
denis <deyilmaz@techfak.de>
2025-05-17 23:15:29 +0200

    updated api keys

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // --- Configuration ---
 const N8N_WEBHOOK_URL = 'https://webhook-processor-production-ef3e.up.railway.app/webhook/7bc992d1-2857-4cb6-b708-b62c162c3f40';
-const ELEVENLABS_API_KEY_DEFAULT = 'sk_7666b5146c9feea10a685abcaa0d034bd89d87105875f411'; // Default key (German, English, Turkish)
-const ELEVENLABS_API_KEY_ARABIC = 'sk_831124d925285659dad240555a312c42cac868c94bc01e35'; // Arabic-specific key
+const ELEVENLABS_API_KEY_DEFAULT = 'sk_9b64b816585c2e5d4571c7ff4c26e546d4562e699dcbaf34'; // Default key (German, English, Turkish)
+const ELEVENLABS_API_KEY_ARABIC = 'sk_fae445a502bdf9d6721db1253e41b2ced35028c85b333e0b'; // Arabic-specific key
 const ELEVENLABS_VOICE_ID_DEFAULT = 'kaGxVtjLwllv1bi2GFag'; // Default German Voice ID
 
 // --- Session ID ---
